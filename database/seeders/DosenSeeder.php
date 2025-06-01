8871a866f739c3135788f937f22b5543b535ed4e
vellzix <jordangamael@gmail.com>
2025-06-01 22:08:09 +0800

    Populate dosen & Mahasiswa

--- database/seeders/DosenSeeder.php
+++ database/seeders/DosenSeeder.php
@@ -11,55 +11,151 @@ class DosenSeeder extends Seeder
 {
     public function run(): void
     {
-        // Dosen 1
-        $user1 = User::create([
-            'name' => 'Dr. Budi Santoso, M.Kom.',
-            'email' => 'budi.santoso@siakad.ac.id',
-            'password' => Hash::make('dosen123'),
-            'role' => 'dosen',
-            'email_verified_at' => now(),
-        ]);
+        $dosens = [
+            [
+                'name' => 'Dr. Budi Santoso, M.Kom.',
+                'email' => 'budi.santoso@siakad.ac.id',
+                'nip' => '198505152010121002',
+                'no_hp' => '081234567801',
+                'tempat_lahir' => 'Jakarta',
+                'tanggal_lahir' => '1985-05-15',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Islam',
+                'prodi_id' => 1,
+                'matakuliah_id' => 1,
+            ],
+            [
+                'name' => 'Dr. Siti Aminah, M.T.',
+                'email' => 'siti.aminah@siakad.ac.id',
+                'nip' => '197708232005012001',
+                'no_hp' => '081234567802',
+                'tempat_lahir' => 'Bandung',
+                'tanggal_lahir' => '1977-08-23',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'matakuliah_id' => 2,
+            ],
+            [
+                'name' => 'Prof. Ahmad Wijaya, Ph.D.',
+                'email' => 'ahmad.wijaya@siakad.ac.id',
+                'nip' => '196505051990031002',
+                'no_hp' => '081234567803',
+                'tempat_lahir' => 'Surabaya',
+                'tanggal_lahir' => '1965-05-05',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Islam',
+                'prodi_id' => 1,
+                'matakuliah_id' => 3,
+            ],
+            [
+                'name' => 'Dr. Rina Kartika, M.Si.',
+                'email' => 'rina.kartika@siakad.ac.id',
+                'nip' => '198012102008012003',
+                'no_hp' => '081234567804',
+                'tempat_lahir' => 'Yogyakarta',
+                'tanggal_lahir' => '1980-12-10',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Kristen',
+                'prodi_id' => 2,
+                'matakuliah_id' => 4,
+            ],
+            [
+                'name' => 'Dr. Hendra Pratama, M.Kom.',
+                'email' => 'hendra.pratama@siakad.ac.id',
+                'nip' => '197503201999031001',
+                'no_hp' => '081234567805',
+                'tempat_lahir' => 'Medan',
+                'tanggal_lahir' => '1975-03-20',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Islam',
+                'prodi_id' => 1,
+                'matakuliah_id' => 5,
+            ],
+            [
+                'name' => 'Dr. Maya Sari, M.T.',
+                'email' => 'maya.sari@siakad.ac.id',
+                'nip' => '198207152010012004',
+                'no_hp' => '081234567806',
+                'tempat_lahir' => 'Palembang',
+                'tanggal_lahir' => '1982-07-15',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'matakuliah_id' => 6,
+            ],
+            [
+                'name' => 'Dr. Agus Setiawan, M.Kom.',
+                'email' => 'agus.setiawan@siakad.ac.id',
+                'nip' => '197809182003121003',
+                'no_hp' => '081234567807',
+                'tempat_lahir' => 'Semarang',
+                'tanggal_lahir' => '1978-09-18',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Katolik',
+                'prodi_id' => 1,
+                'matakuliah_id' => 7,
+            ],
+            [
+                'name' => 'Dr. Dewi Lestari, M.Si.',
+                'email' => 'dewi.lestari@siakad.ac.id',
+                'nip' => '198411252012012005',
+                'no_hp' => '081234567808',
+                'tempat_lahir' => 'Makassar',
+                'tanggal_lahir' => '1984-11-25',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'matakuliah_id' => 8,
+            ],
+            [
+                'name' => 'Dr. Rudi Hermawan, M.T.',
+                'email' => 'rudi.hermawan@siakad.ac.id',
+                'nip' => '197601302001121004',
+                'no_hp' => '081234567809',
+                'tempat_lahir' => 'Denpasar',
+                'tanggal_lahir' => '1976-01-30',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Hindu',
+                'prodi_id' => 1,
+                'matakuliah_id' => 9,
+            ],
+            [
+                'name' => 'Dr. Fitri Handayani, M.Kom.',
+                'email' => 'fitri.handayani@siakad.ac.id',
+                'nip' => '198906142015012006',
+                'no_hp' => '081234567810',
+                'tempat_lahir' => 'Padang',
+                'tanggal_lahir' => '1989-06-14',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'matakuliah_id' => 10,
+            ],
+        ];
 
-        Dosen::create([
-            'user_id' => $user1->id,
-            'nip' => '198505152010121002',
-            'nama' => $user1->name,
-            'bidang_keahlian' => 'Artificial Intelligence',
-            'gelar_akademik' => 'Dr.',
-        ]);
+        foreach ($dosens as $dosenData) {
+            $user = User::create([
+                'name' => $dosenData['name'],
+                'email' => $dosenData['email'],
+                'password' => Hash::make('password123'),
+                'role' => 'dosen',
+                'email_verified_at' => now(),
+            ]);
 
-        // Dosen 2
-        $user2 = User::create([
-            'name' => 'Dr. Siti Aminah, M.T.',
-            'email' => 'siti.aminah@siakad.ac.id',
-            'password' => Hash::make('dosen123'),
-            'role' => 'dosen',
-            'email_verified_at' => now(),
-        ]);
-
-        Dosen::create([
-            'user_id' => $user2->id,
-            'nip' => '197708232005012001',
-            'nama' => $user2->name,
-            'bidang_keahlian' => 'Software Engineering',
-            'gelar_akademik' => 'Dr.',
-        ]);
-
-        // Dosen 3
-        $user3 = User::create([
-            'name' => 'Prof. Ahmad Wijaya, Ph.D.',
-            'email' => 'ahmad.wijaya@siakad.ac.id',
-            'password' => Hash::make('dosen123'),
-            'role' => 'dosen',
-            'email_verified_at' => now(),
-        ]);
-
-        Dosen::create([
-            'user_id' => $user3->id,
-            'nip' => '196505051990031002',
-            'nama' => $user3->name,
-            'bidang_keahlian' => 'Database Systems',
-            'gelar_akademik' => 'Prof.',
-        ]);
+            Dosen::create([
+                'user_id' => $user->id,
+                'nip' => $dosenData['nip'],
+                'nama' => $dosenData['name'],
+                'no_hp' => $dosenData['no_hp'],
+                'tempat_lahir' => $dosenData['tempat_lahir'],
+                'tanggal_lahir' => $dosenData['tanggal_lahir'],
+                'jenis_kelamin' => $dosenData['jenis_kelamin'],
+                'agama' => $dosenData['agama'],
+                'prodi_id' => $dosenData['prodi_id'],
+                'matakuliah_id' => $dosenData['matakuliah_id'],
+                'status' => 'Aktif',
+            ]);
+        }
     }
 }
--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -11,76 +11,151 @@ class MahasiswaSeeder extends Seeder
 {
     public function run(): void
     {
-        // Mahasiswa 1
-        $user1 = User::create([
-            'name' => 'Andi Pratama',
-            'email' => 'andi.pratama@mhs.siakad.ac.id',
-            'password' => Hash::make('mahasiswa123'),
-            'role' => 'mahasiswa',
-            'email_verified_at' => now(),
-        ]);
+        $mahasiswas = [
+            [
+                'name' => 'Andi Pratama',
+                'email' => 'andi.pratamas@mhs.siakad.ac.id',
+                'nim' => '2024010011',
+                'no_hp' => '081234567901',
+                'tempat_lahir' => 'Jakarta',
+                'tanggal_lahir' => '2003-01-15',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Islam',
+                'prodi_id' => 1,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Dewi Lestari',
+                'email' => 'dewi.lestari@mhs.siakad.ac.id',
+                'nim' => '2024010002',
+                'no_hp' => '081234567902',
+                'tempat_lahir' => 'Bandung',
+                'tanggal_lahir' => '2003-05-22',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Muhammad Rizki',
+                'email' => 'muhammad.rizki@mhs.siakad.ac.id',
+                'nim' => '2024010003',
+                'no_hp' => '081234567903',
+                'tempat_lahir' => 'Surabaya',
+                'tanggal_lahir' => '2003-08-10',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Islam',
+                'prodi_id' => 1,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Sarah Putri',
+                'email' => 'sarah.putri@mhs.siakad.ac.id',
+                'nim' => '2024010004',
+                'no_hp' => '081234567904',
+                'tempat_lahir' => 'Yogyakarta',
+                'tanggal_lahir' => '2003-12-03',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Kristen',
+                'prodi_id' => 2,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Bayu Setiawan',
+                'email' => 'bayu.setiawan@mhs.siakad.ac.id',
+                'nim' => '2024010005',
+                'no_hp' => '081234567905',
+                'tempat_lahir' => 'Medan',
+                'tanggal_lahir' => '2003-03-18',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Islam',
+                'prodi_id' => 1,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Sari Indah',
+                'email' => 'sari.indah@mhs.siakad.ac.id',
+                'nim' => '2024010006',
+                'no_hp' => '081234567906',
+                'tempat_lahir' => 'Palembang',
+                'tanggal_lahir' => '2003-07-25',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Dimas Aditya',
+                'email' => 'dimas.aditya@mhs.siakad.ac.id',
+                'nim' => '2024010007',
+                'no_hp' => '081234567907',
+                'tempat_lahir' => 'Semarang',
+                'tanggal_lahir' => '2003-11-12',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Katolik',
+                'prodi_id' => 1,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Putri Maharani',
+                'email' => 'putri.maharani@mhs.siakad.ac.id',
+                'nim' => '2024010008',
+                'no_hp' => '081234567908',
+                'tempat_lahir' => 'Makassar',
+                'tanggal_lahir' => '2003-04-08',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Arief Wicaksono',
+                'email' => 'arief.wicaksono@mhs.siakad.ac.id',
+                'nim' => '2024010009',
+                'no_hp' => '081234567909',
+                'tempat_lahir' => 'Denpasar',
+                'tanggal_lahir' => '2003-09-30',
+                'jenis_kelamin' => 'L',
+                'agama' => 'Hindu',
+                'prodi_id' => 1,
+                'angkatan' => '2024',
+            ],
+            [
+                'name' => 'Nurul Fadilah',
+                'email' => 'nurul.fadilah@mhs.siakad.ac.id',
+                'nim' => '2024010010',
+                'no_hp' => '081234567910',
+                'tempat_lahir' => 'Padang',
+                'tanggal_lahir' => '2003-06-14',
+                'jenis_kelamin' => 'P',
+                'agama' => 'Islam',
+                'prodi_id' => 2,
+                'angkatan' => '2024',
+            ],
+        ];
 
-        Mahasiswa::create([
-            'user_id' => $user1->id,
-            'nim' => '2024010001',
-            'nama' => $user1->name,
-            'angkatan' => '2024',
-            'program_studi' => 'Teknik Informatika',
-            'status' => 'Aktif',
-        ]);
+        foreach ($mahasiswas as $mahasiswaData) {
+            $user = User::create([
+                'name' => $mahasiswaData['name'],
+                'email' => $mahasiswaData['email'],
+                'password' => Hash::make('password123'),
+                'role' => 'mahasiswa',
+                'email_verified_at' => now(),
+            ]);
 
-        // Mahasiswa 2
-        $user2 = User::create([
-            'name' => 'Dewi Lestari',
-            'email' => 'dewi.lestari@mhs.siakad.ac.id',
-            'password' => Hash::make('mahasiswa123'),
-            'role' => 'mahasiswa',
-            'email_verified_at' => now(),
-        ]);
-
-        Mahasiswa::create([
-            'user_id' => $user2->id,
-            'nim' => '2024010002',
-            'nama' => $user2->name,
-            'angkatan' => '2024',
-            'program_studi' => 'Sistem Informasi',
-            'status' => 'Aktif',
-        ]);
-
-        // Mahasiswa 3
-        $user3 = User::create([
-            'name' => 'Muhammad Rizki',
-            'email' => 'muhammad.rizki@mhs.siakad.ac.id',
-            'password' => Hash::make('mahasiswa123'),
-            'role' => 'mahasiswa',
-            'email_verified_at' => now(),
-        ]);
-
-        Mahasiswa::create([
-            'user_id' => $user3->id,
-            'nim' => '2024010003',
-            'nama' => $user3->name,
-            'angkatan' => '2024',
-            'program_studi' => 'Teknik Informatika',
-            'status' => 'Aktif',
-        ]);
-
-        // Mahasiswa 4
-        $user4 = User::create([
-            'name' => 'Sarah Putri',
-            'email' => 'sarah.putri@mhs.siakad.ac.id',
-            'password' => Hash::make('mahasiswa123'),
-            'role' => 'mahasiswa',
-            'email_verified_at' => now(),
-        ]);
-
-        Mahasiswa::create([
-            'user_id' => $user4->id,
-            'nim' => '2024010004',
-            'nama' => $user4->name,
-            'angkatan' => '2024',
-            'program_studi' => 'Sistem Informasi',
-            'status' => 'Aktif',
-        ]);
+            Mahasiswa::create([
+                'user_id' => $user->id,
+                'nim' => $mahasiswaData['nim'],
+                'nama' => $mahasiswaData['name'],
+                'no_hp' => $mahasiswaData['no_hp'],
+                'tempat_lahir' => $mahasiswaData['tempat_lahir'],
+                'tanggal_lahir' => $mahasiswaData['tanggal_lahir'],
+                'jenis_kelamin' => $mahasiswaData['jenis_kelamin'],
+                'agama' => $mahasiswaData['agama'],
+                'prodi_id' => $mahasiswaData['prodi_id'],
+                'angkatan' => $mahasiswaData['angkatan'],
+                'status' => 'Aktif',
+            ]);
+        }
     }
 }
